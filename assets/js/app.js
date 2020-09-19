8c568a5ac64ab0b49ba9e86e02e0b28e1c33d485
ifancyabroad <edgar.nightingale@btinternet.com>
2020-09-19 18:30:24 +0100

    Make full screen menu responsive

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,46 +1,49 @@
 $(document).ready(() => {
-  // Fade scroll to anchor links
-  const links = document.querySelectorAll('a.transition');
-  $(links).on('click', function(e) {
-    e.preventDefault();
-    $(document.body).fadeOut(500, redirectPage.bind(this, this.href));
-  })
 
-  const redirectPage = (link) => {
-    closeMenu(0);
-    $(document.body).fadeIn(500);
-    setTimeout(() => {
-      window.location = link;
-    }, 0);
-  }
-
-  // Menu Code
-  const menu = document.querySelector('.menu-modal');
-  const menuBtnContainer = document.querySelector('.menu-btn-container');
-  const menuBtn = document.querySelector('.menu-btn');
-  const menuCloseBtn = document.querySelector('.close-menu');
+  // Fullscreen menu
+  const documentElement = $(document.documentElement);
+  const body = $(document.body);
+  const menu = $('.menu-modal');
+  const menuBtnContainer = $('.menu-btn-container');
+  const menuBtn = $('.menu-btn');
+  const menuCloseBtn = $('.close-menu');
   let menuOpen = false;
 
   const openMenu = (time = 400) => {
     if (!menuOpen) {
-      document.documentElement.classList.add('modal-open');
-      document.body.classList.add('modal-open');
-      menuBtn.classList.add('open');
-      $(menu).fadeIn(time);
+      documentElement.addClass('modal-open');
+      body.addClass('modal-open');
+      menuBtn.addClass('open');
+      menu.fadeIn(time);
       menuOpen = true;
     }
   }
 
   const closeMenu = (time = 400) => {
     if (menuOpen) {
-      document.documentElement.classList.remove('modal-open');
-      document.body.classList.remove('modal-open');
-      menuBtn.classList.remove('open');
-      $(menu).fadeOut(time);
+      documentElement.removeClass('modal-open');
+      body.removeClass('modal-open');
+      menuBtn.removeClass('open');
+      menu.fadeOut(time);
       menuOpen = false;
     }
   }
 
-  menuBtnContainer.addEventListener('click', openMenu);
-  menuCloseBtn.addEventListener('click', closeMenu);
+  menuBtnContainer.on('click', openMenu);
+  menuCloseBtn.on('click', closeMenu);
+
+  // Fade scroll to anchor links
+  const links = $('a.transition');
+  links.on('click', function(e) {
+    e.preventDefault();
+    body.fadeOut(500, redirectPage.bind(this, this.href));
+  })
+
+  const redirectPage = (link) => {
+    closeMenu(0);
+    body.fadeIn(500);
+    setTimeout(() => {
+      window.location = link;
+    }, 0);
+  }
 })
