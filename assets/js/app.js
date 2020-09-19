43c661f478c6ae7477318739ec1a1e8a9d8fb736
ifancyabroad <edgar.nightingale@btinternet.com>
2020-09-19 17:32:18 +0100

    Add menu modal and separate html into separate files

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,46 @@
-// Menu
-const menuBtn = document.querySelector('.menu-btn');
-let menuOpen = false;
-menuBtn.addEventListener('click', () => {
-  if(!menuOpen) {
-    menuBtn.classList.add('open');
-    menuOpen = true;
-  } else {
-    menuBtn.classList.remove('open');
-    menuOpen = false;
+$(document).ready(() => {
+  // Fade scroll to anchor links
+  const links = document.querySelectorAll('a.transition');
+  $(links).on('click', function(e) {
+    e.preventDefault();
+    $(document.body).fadeOut(500, redirectPage.bind(this, this.href));
+  })
+
+  const redirectPage = (link) => {
+    closeMenu(0);
+    $(document.body).fadeIn(500);
+    setTimeout(() => {
+      window.location = link;
+    }, 0);
   }
-});
+
+  // Menu Code
+  const menu = document.querySelector('.menu-modal');
+  const menuBtnContainer = document.querySelector('.menu-btn-container');
+  const menuBtn = document.querySelector('.menu-btn');
+  const menuCloseBtn = document.querySelector('.close-menu');
+  let menuOpen = false;
+
+  const openMenu = (time = 400) => {
+    if (!menuOpen) {
+      document.documentElement.classList.add('modal-open');
+      document.body.classList.add('modal-open');
+      menuBtn.classList.add('open');
+      $(menu).fadeIn(time);
+      menuOpen = true;
+    }
+  }
+
+  const closeMenu = (time = 400) => {
+    if (menuOpen) {
+      document.documentElement.classList.remove('modal-open');
+      document.body.classList.remove('modal-open');
+      menuBtn.classList.remove('open');
+      $(menu).fadeOut(time);
+      menuOpen = false;
+    }
+  }
+
+  menuBtnContainer.addEventListener('click', openMenu);
+  menuCloseBtn.addEventListener('click', closeMenu);
+})
